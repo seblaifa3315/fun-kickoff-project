939a332d596ad678685fe267fa1489eb9db28510
Sebastien Laifa <seblaifa@hotmail.com>
2022-01-20 09:47:08 -0800

    profile model

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,9 +22,7 @@ function App(): JSX.Element {
               <Switch>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/signup" component={Signup} />
-                <Route exact path="/dashboard">
-                  <Dashboard />
-                </Route>
+                <Route exact path="/dashboard" component={Dashboard} />
                 <Route path="*">
                   <Redirect to="/login" />
                 </Route>
